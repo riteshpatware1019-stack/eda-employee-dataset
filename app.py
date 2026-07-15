971dae19bc38da719c53101a2f121e2da825ee38
riteshpatware1019-stack <riteshpatware1019@gmail.com>
2026-07-15 16:12:47 +0530

    Make Dashboard

--- app.py
+++ app.py
@@ -72,10 +72,83 @@ st.subheader("Duplicate Rows")
 st.write(df.duplicated().sum())
 
 
-dept=filtered.groupby("Department")["Salary"].mean()
-st.dataframe(dept)
-fig,ax=plt.subplots(figsize=(8,5))
+dept = filtered.groupby("Department")["Salary"].mean()
+fig, ax = plt.subplots(figsize=(8,5)) 
+
 ax.bar(dept.index,dept.values)
 ax.set_title("Average Salary by Department")
 plt.xticks(rotation=45)
 st.pyplot(fig)
+
+performance = filtered["Performance"].value_counts()
+fig, ax = plt.subplots(figsize=(6,6))
+
+ax.pie(
+    performance,
+    labels=performance.index,
+    autopct="%1.1f%%",
+)
+ax.set_title("Performance Distribution")
+st.pyplot(fig)
+
+
+skill = filtered["SkillScore"]
+fig, ax = plt.subplots(figsize=(6,5))
+
+ax.boxplot(skill)
+ax.set_title("Skill Score Distribution")
+st.pyplot(fig)
+
+
+city = filtered["City"].value_counts()
+fig, ax = plt.subplots(figsize=(8,5))
+
+ax.barh(city.index, city.values, color="orange")
+ax.set_title("Employee Count by City")
+ax.set_xlabel("Employees")
+ax.set_ylabel("City")
+st.pyplot(fig)
+
+salary =filtered["Salary"]
+experience=filtered["Experience"]
+fig, ax = plt.subplots(figsize=(8,5))
+
+ax.scatter(
+    salary,
+    experience,
+    color="red",
+    alpha=0.7
+)
+ax.set_title("Salary vs Experience")
+ax.set_xlabel("Experience (Years)")
+ax.set_ylabel("Salary")
+st.pyplot(fig)
+
+
+st.subheader("Highest Salary Employee")
+st.dataframe(
+    filtered.nlargest(1,"Salary")
+)
+
+st.subheader("Lowest Salary Employee")
+st.dataframe(
+    filtered.nsmallest(1,"Salary")
+)
+
+st.subheader("Average Salary")
+st.write(filtered["Salary"].mean())
+
+st.subheader("Correlation Matrix")
+st.dataframe(
+    filtered.corr(numeric_only=True)
+)
+
+
+csv=filtered.to_csv(index=False)
+
+st.download_button(
+    "Download CSV",
+    csv,
+    "employees.csv",
+    "text/csv"
+)
